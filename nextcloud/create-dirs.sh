a8162d3fff7ea23832add339262a03cf9e98a6c2
Rajeev Sharma <rajeev1982@gmail.com>
2025-03-20 18:27:51 +0530

    Added script

---
+++ nextcloud/create-dirs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+root_folder=/opt/kubernetes-volumes/nextcloud.test
+
+rm -rf 		                ${root_folder}
+mkdir -p                    ${root_folder}
+
+mkdir 				        ${root_folder}/apps ${root_folder}/config ${root_folder}/data ${root_folder}/html
+chown -R www-data:www-data	${root_folder}/apps ${root_folder}/config ${root_folder}/data ${root_folder}/html
+chmod -R 0770			    ${root_folder}/apps ${root_folder}/config ${root_folder}/data ${root_folder}/html
+
+mkdir 				        ${root_folder}/mariadb ${root_folder}/redis
+chown -R www-data:www-data	${root_folder}/mariadb ${root_folder}/redis
+chmod -R 0777			    ${root_folder}/mariadb ${root_folder}/redis
