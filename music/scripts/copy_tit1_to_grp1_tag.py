89795cc5a1123431ddfb9d59e34e01fb0148bfd6
Rajeev Sharma <rajeev1982@gmail.com>
2025-09-16 21:34:02 +0530

    Added music tag management script

--- music/scripts/copy_tit1_to_grp1_tag.py
+++ music/scripts/copy_tit1_to_grp1_tag.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import eyed3
 from eyed3.id3.apple import GRP1
 
@@ -13,6 +15,14 @@ class IDE3TagManager:
         self.__audiofile = audiofile
         if not self.__audiofile.tag:
             self.__audiofile.initTag()
+
+    def get_tag_version(self) -> tuple[int, int, int] | None:
+        if self.__audiofile.tag.version:  # type: ignore
+            print(f"ID3 Tag version for file {self.__audiofile.path}: {self.__audiofile.tag.version}")  # type: ignore
+            return self.__audiofile.tag.version  # type: ignore
+        else:
+            print(f"No ID3 Tag version found for file {self.__audiofile.path}")
+            return None
 
     def does_tit1_exist(self) -> bool:
         return bool(
@@ -41,32 +51,75 @@ class IDE3TagManager:
             return False
 
     def copy_tit1_to_grp1(self) -> None:
-        del self.__audiofile.tag.frame_set[b"GRP1"]  # type: ignore
+        if self.does_grp1_exist():
+            del self.__audiofile.tag.frame_set[b"GRP1"]  # type: ignore
 
         if not self.does_tit1_exist():
             print(f"No TIT1 tag found for file: {self.__audiofile.path} ")
             return
 
         for tit1 in self.__audiofile.tag.frame_set[b"TIT1"]:  # type: ignore # type: ignore
-            # print(f"TIT1: {tit1.data}")
             grp1: GRP1 = GRP1()
-            grp1.__dict__ = self.__audiofile.tag.frame_set[b"TIT1"][0].__dict__.copy()  # type: ignore
+            grp1.__dict__ = tit1.__dict__.copy()  # type: ignore
             grp1.id = b"GRP1"
             self.__audiofile.tag.frame_set[b"GRP1"] = grp1  # type: ignore
 
-        self.__audiofile.tag.save()  # type: ignore
+        v_major: int
+        v_minor: int
+        v_rivision: int
+        v_major, v_minor, v_rivision = self.get_tag_version() or (0, 0, 0)
+        if v_major < 2 or (v_major == 2 and v_minor < 3):
+            self.__audiofile.tag.save(version=(2, 3, 0))  # type: ignore
+        else:
+            self.__audiofile.tag.save()  # type: ignore
+
         print(f"Copied TIT1 to GRP1 for file: {self.__audiofile.path}")
 
 
+class MusicManager:
+    __folderpath: str | None
+
+    def __init__(self, folderpath: str) -> None:
+        self.__folderpath = folderpath
+
+        if not folderpath or not os.path.exists(folderpath) or not os.path.isdir(folderpath):
+            raise Exception(f"Folder path does not exist: {folderpath}")
+
+    def update_ide3_tags(self) -> None:
+        self.update_ide3_tags_for_folder(self.__folderpath)
+
+    def update_ide3_tags_for_folder(self, folderpath: str | None) -> None:
+        if not folderpath or not os.path.exists(folderpath) or not os.path.isdir(folderpath):
+            raise Exception(f"Folder path does not exist: {folderpath}")
+
+        for root, folders, files in os.walk(folderpath):
+            for folder in folders:
+                folderpath = os.path.join(root, folder)
+                print(f"Processing folder: {folderpath}")
+                self.update_ide3_tags_for_folder(folderpath)
+
+            for file in files:
+                if file.lower().endswith((".mp3")):
+                    filepath = os.path.join(root, file)
+                    print(f"Processing file: {filepath}")
+                    try:
+                        self.update_ide3_tags_for_file(filepath)
+                    except Exception as e:
+                        print(f"Error processing file {filepath}: {e}")
+
+    def update_ide3_tags_for_file(self, filepath: str) -> None:
+        tag_manager = IDE3TagManager(filepath)
+        tag_manager.copy_tit1_to_grp1()
+
+
 if __name__ == "__main__":
-    # import sys
+    if len(sys.argv) != 2:
+        print("Usage: python copy_tit1_to_grp1_tag.py <path_to_music_folder>")
+        sys.exit(1)
 
-    # if len(sys.argv) != 2:
-    #     print("Usage: python copy_tit1_to_grp1_tag.py <path_to_music_folder>")
-    #     sys.exit(1)
+    folderpath = sys.argv[1]
+    music_tag_manager = MusicManager(folderpath)
+    music_tag_manager.update_ide3_tags()
 
-    # folderpath = sys.argv[1]
-
-    filepath: str = "/Users/rajeev/Music/DJ Music/Hindi/Dance Songs/Besharam Rang.mp3"
-    tag_manager = IDE3TagManager(filepath)
-    tag_manager.copy_tit1_to_grp1()
+    # IDE3TagManager("/Users/rajeev/DJ Music/Hindi/Dandiya Remixes/Piya Se Milan Gai.mp3").copy_tit1_to_grp1()
+    # tag_manager = IDE3TagManager("/Users/rajeev/DJ Music/Hindi/Dandiya Remixes/Piya Se Milan Gai.mp3").get_tag_version()
